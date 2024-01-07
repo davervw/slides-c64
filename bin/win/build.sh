afe1c2fe9cfc604c410fe67a5313a59a9d35267d
Dave Van Wagner <git@davevw.com>
2024-01-07 12:46:15 -0800

    errors to file instead of terminal

--- bin/win/build.sh
+++ bin/win/build.sh
@@ -1,7 +1,8 @@
 export ACME=/c/Users/Dave/Downloads/acme0.97win/acme
 export VICE=/c/Users/Dave/Downloads/GTK3VICE-3.6.1-win64/bin
-${ACME}/acme.exe -f cbm -l build/labels -o build/lores.ml.prg code/lores.asm
+${ACME}/acme.exe -f cbm -l build/labels -o build/lores.ml.prg code/lores.asm 2> build/build.err
 [ $? -eq 0 ] || exit 1
+rm build/build.err
 [ $? -eq 0 ] && ${VICE}/c1541 << EOF
 attach build/slides.d64
 delete lores.ml
