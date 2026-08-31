48decb7ae404eeba066d127827391869f36a7612
Dave Van Wagner <git@davevw.com>
2026-08-30 20:51:58 -0700

    rleplayer.ml (64): exit verification screen - press x to exit

--- code/rleplayer.asm
+++ code/rleplayer.asm
@@ -2,6 +2,7 @@
 
 chrout = $FFD2 ; character out to device (normally screen)
 getin = $FFE4 ; read character from keyboard, 0=no key
+plot = $FFF0 ; C clear sets cursor position X=row, Y=col, or if C set gets cursor position
 
 src_p=$FB
 src_h=$FC
@@ -14,9 +15,15 @@ value=$24
 len=$25
 pages=$26 ; count of pages indexed
 page=$27 ; 0 based
+save_border=$28
+save_background=$29
+save_foreground=$2a
 
 screen_index = $33C
 max_pages = 96
+
+text_addr=$0400
+color_addr=$d800
 
 ; 10 SYS 2062
 !byte <end_basic, >end_basic
@@ -25,11 +32,17 @@ end_basic:
 !byte 0
 
 screen_player:
+    jmp main
+
+set_slide_colors:
     lda #$0e
     ldx #$06
-    sta $d020
-    stx $d021
-    sta 646
+    ldy #$0e
+    jmp set_colors
+
+main:
+    jsr save_colors
+    jsr set_slide_colors
     lda #$93
     jsr chrout
     lda #<start_data
@@ -41,7 +54,7 @@ screen_loop:
     lda page
     cmp pages
     bcc +
-    rts
+    jmp exit_basic
 +   asl
     tay
     lda screen_index,y
@@ -51,28 +64,37 @@ screen_loop:
     lda #2
     ldx #0
     jsr addto_src_p
-    lda #$00
-    ldx #$d8
+    lda #<color_addr
+    ldx #>color_addr
     sta dst_p
     stx dst_h
     jsr decode_color
-    lda #$00
-    ldx #$04
+    lda #<text_addr
+    ldx #>text_addr
     sta dst_p
     stx dst_h
     jsr decode_text
 -   jsr getin
     cmp #$00
     beq -
-    cmp #3
+    cmp #'X'
+    bne +
+    jmp exit_basic_x
++   cmp #3
     bne +
     lda #$93
-    jmp chrout
+    jsr chrout
+    jmp exit_basic
 +   cmp #20 ; delete
     bne +
 --  lda page ; check if can backup a page
     beq -
     dec page
+    jmp ++
++   cmp #19 ; home
+    bne +
+    lda #0
+    sta page
     jmp ++
 +   cmp #157 ; left
     beq --
@@ -304,5 +326,91 @@ addto_src_p: ; A low, X high, perform 16-bit addition from/to src_p/src_h
     adc src_h
     sta src_h
     rts
+
+save_colors:
+    lda $d020
+    ldx $d021
+    ldy 646
+    sta save_border
+    stx save_background
+    sty save_foreground
+    rts
+
+exit_basic:
+    lda #0
+    tax
+    ldy #1
+    jsr set_colors
+    ldx #12
+    ldy #(20-exit_message_len/2)
+    clc
+    jsr plot
+    lda #<exit_message
+    ldx #>exit_message
+    jsr string_out
+--  jsr getin
+    cmp #0
+    beq --
+    cmp #19
+    bne ++
+    lda #0
+    sta page
+-   lda #$93
+    jsr chrout
+    jsr set_slide_colors
+    lda page
+    cmp pages
+    bcc +
+    dec page
++   jmp screen_loop
+++  cmp #20 ; del (backspace)
+    beq -
+    cmp #157 ; left
+    beq -
+    cmp #145 ; up
+    beq -
+    cmp #'1'
+    bcc +
+    cmp #('9'+1)
+    bcs +
+    sec
+    sbc #'1'
+    cmp pages
+    bcs --
+    sta page
+    jmp -
++   cmp #'X'
+    bne --
+exit_basic_x:    
+    lda save_border
+    ldx save_background
+    ldy save_foreground
+    jsr set_colors
+    lda #$93
+    jmp chrout ; and exit to BASIC
+
+set_colors:
+    sta $d020
+    stx $d021
+    sty 646
+    rts
+
+string_out:
+    sta src_p
+    stx src_h
+    ldy #0
+-   lda (src_p), y
+    beq +
+    jsr chrout
+    iny
+    bne -
+    inc src_h
+    jmp -
++   rts
+
+exit_message:
+    !text "(PRESS X TO EXIT)"
+exit_message_len = * - exit_message    
+    !byte 0
 
 start_data:
